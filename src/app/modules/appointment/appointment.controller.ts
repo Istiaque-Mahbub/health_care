6c00cbdc3aa3c66adfd12247c083e9847b0cc494
Istiaque Mahbub Isti <istiaquemahbu1@gmail.com>
2026-05-10 13:27:17 +0600

    Add getAllFromDB function to retrieve appointments with filtering options

--- src/app/modules/appointment/appointment.controller.ts
+++ src/app/modules/appointment/appointment.controller.ts
@@ -3,6 +3,8 @@ import catchAsync from "../../shared/catchAsync";
 import sendResponse from "../../shared/sendResponse";
 import { AppointmentService } from "./appointment.service";
 import pick from "../../helper/pick";
+import { appointmentFilterableFields } from "./appointment.constant";
+import httpStatus from "http-status";
 
 const createAppointment = catchAsync(async (req: Request & { user?: any }, res: Response) => {
     const user = req.user;
@@ -46,8 +48,22 @@ const updateAppointmentStatus = catchAsync(async (req: Request & { user?: any },
     })
 })
 
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    const filters = pick(req.query, appointmentFilterableFields)
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+    const result = await AppointmentService.getAllFromDB(filters, options);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Appointment retrieval successfully',
+        meta: result.meta,
+        data: result.data,
+    });
+});
+
 export const AppointmentController ={
     createAppointment,
     getMyAppointment,
-    updateAppointmentStatus
+    updateAppointmentStatus,
+    getAllFromDB
 }
