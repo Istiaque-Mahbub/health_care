c24883217716b2857fec92f99e385bed725f0180
Istiaque Mahbub Isti <istiaquemahbu1@gmail.com>
2026-05-10 13:26:28 +0600

    Add getAllFromDB function to retrieve reviews with filtering by patient and doctor emails

--- src/app/modules/review/review,service.ts
+++ src/app/modules/review/review,service.ts
@@ -1,6 +1,8 @@
+import { Prisma } from "@prisma/client";
 import ApiError from "../../errors/ApiError";
 import { prisma } from "../../shared/prisma";
 import httpStatus  from 'http-status';
+import { paginationHelper } from "../../helper/paginationHelper";
 
 const insertIntoDB = async (user: any, payload: any) => {
     const patientData = await prisma.patient.findUniqueOrThrow({
@@ -52,6 +54,64 @@ const insertIntoDB = async (user: any, payload: any) => {
     })
 };
 
+const getAllFromDB = async (
+    filters: any,
+    options: any,
+) => {
+    const { limit, page, skip } = paginationHelper.calculatedPagination(options);
+    const { patientEmail, doctorEmail } = filters;
+    const andConditions = [];
+
+    if (patientEmail) {
+        andConditions.push({
+            patient: {
+                email: patientEmail
+            }
+        })
+    }
+
+    if (doctorEmail) {
+        andConditions.push({
+            doctor: {
+                email: doctorEmail
+            }
+        })
+    }
+
+    const whereConditions: Prisma.ReviewWhereInput =
+        andConditions.length > 0 ? { AND: andConditions } : {};
+
+    const result = await prisma.review.findMany({
+        where: whereConditions,
+        skip,
+        take: limit,
+        orderBy:
+            options.sortBy && options.sortOrder
+                ? { [options.sortBy]: options.sortOrder }
+                : {
+                    createdAt: 'desc',
+                },
+        include: {
+            doctor: true,
+            patient: true,
+            //appointment: true,
+        },
+    });
+    const total = await prisma.review.count({
+        where: whereConditions,
+    });
+
+    return {
+        meta: {
+            total,
+            page,
+            limit,
+        },
+        data: result,
+    };
+};
+
 export const ReviewService = {
-    insertIntoDB
+    insertIntoDB,
+    getAllFromDB
 }
